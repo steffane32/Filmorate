da7dd84218a82dd8d9d8eda3936393de2ef670fa
Olga Zavalishina <steffane@yandex.ru>
2026-01-09 15:36:30 +0300

    sprint 11, homework_1.4 удалены лишние импорты в тестах

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
