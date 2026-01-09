3df5303dbfa7c4458eb60cea214fa88f39effdc8
Olga Zavalishina <steffane@yandex.ru>
2026-01-09 15:33:44 +0300

    sprint 11, homework_1.3 добавлены пробелы

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 
 public interface UserStorage {
+
     List<User> findAll();
+
     User create(User user);
+
     User update(User user);
+
     User findById(Long id);
+
     void delete(Long id);
 }
