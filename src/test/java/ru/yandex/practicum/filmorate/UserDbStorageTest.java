8462cd8917060d3cc60cbfe641d4e5373c372351
Olga Zavalishina <steffane@yandex.ru>
2026-01-11 04:37:36 +0300

    sprint 12, homework_1.12 check style по ощущениям - не супер, но оно работает, в БД пишет.

--- src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
