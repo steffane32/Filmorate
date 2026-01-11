ae19868b2f033a82340e064ae55dfb241fd56058
Olga Zavalishina <steffane@yandex.ru>
2026-01-11 04:30:20 +0300

    sprint 12, homework_1.7 check style по ощущениям - не супер, но оно работает, в БД пишет.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/GenreStorage.java
@@ -6,15 +6,23 @@ import java.util.Optional;
 
 public interface GenreStorage {
     List<Genre> findAll();
+
     Optional<Genre> findById(Long id);
+
     Optional<Genre> findByName(String name);
+
     Genre create(Genre genre);
+
     Genre update(Genre genre);
+
     void delete(Long id);
+
     boolean existsById(Long id);
 
     // Методы для работы с фильмами
     List<Genre> getFilmGenres(Long filmId);
+
     void addGenreToFilm(Long filmId, Long genreId);
+
     void removeGenreFromFilm(Long filmId, Long genreId);
 }
