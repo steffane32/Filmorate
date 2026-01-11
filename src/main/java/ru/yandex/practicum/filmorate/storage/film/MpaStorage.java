c0715c84aa8a8fc457125584074c44c5feaa9c61
Olga Zavalishina <steffane@yandex.ru>
2026-01-11 04:31:47 +0300

    sprint 12, homework_1.8 check style по ощущениям - не супер, но оно работает, в БД пишет.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/MpaStorage.java
@@ -5,10 +5,16 @@ import java.util.Optional;
 
 public interface MpaStorage {
     List<Mpa> findAll();
+
     Optional<Mpa> findById(Long id);
+
     Optional<Mpa> findByName(String name);
+
     Mpa create(Mpa mpa);
+
     Mpa update(Mpa mpa);
+
     void delete(Long id);
+
     boolean existsById(Long id);
 }
