42211dbe40b656a5c8561b02e82d1d7f20630a6f
Olga Zavalishina <steffane@yandex.ru>
2026-01-11 04:33:54 +0300

    sprint 12, homework_1.8 check style по ощущениям - не супер, но оно работает, в БД пишет.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,23 +5,34 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     List<User> findAll();
+
     User create(User user);
+
     User update(User user);
+
     User findById(Long id);
+
     void delete(Long id);
 
     // Методы для работы с друзьями (односторонняя дружба)
     void addFriend(Long userId, Long friendId);
+
     void removeFriend(Long userId, Long friendId);
+
     List<User> getFriends(Long userId);
+
     List<User> getCommonFriends(Long userId1, Long userId2);
 
     // Дополнительные методы
     boolean existsById(Long id);
+
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+
     long count();
     List<User> findUsersByIds(List<Long> userIds);
+
     void confirmFriendship(Long userId, Long friendId);
 }
